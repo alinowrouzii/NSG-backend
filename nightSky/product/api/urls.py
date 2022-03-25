952b0f878f6486834aae702ab1472796f7618285
alinowrouzii <alinowrouzii@gmail.com>
2022-03-25 15:22:17 +0430

    Add new app

--- nightSky/product/api/urls.py
+++ nightSky/product/api/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
-from product.api.views import *
+from product.api.views import (
+    ColorAPIView,
+    FontAPIView,
+    DesignAPIView,
+    MapDesignAPIView,
+    ProductModelAPIView,
+)
 
 urlpatterns = [
-   path('color/', ColorAPIView.as_view({"get":"list"})),
-   path('color/<pk>/', ColorAPIView.as_view({"get":"retrieve"})),
-   path('font/', FontAPIView.as_view({"get":"list"})),
-   path('font/<pk>/', FontAPIView.as_view({"get":"retrieve"})),
-   path('design/', DesignAPIView.as_view({"get":"list"})),
-   path('design/<pk>/', DesignAPIView.as_view({"get":"retrieve"})),
-   path('map_design/', MapDesignAPIView.as_view({"get":"list"})),
-   path('map_design/<pk>/', MapDesignAPIView.as_view({"get":"retrieve"})),
-   path('product_model/', ProductModelAPIView.as_view({"get":"list"})),
-   path('product_model/<pk>/', ProductModelAPIView.as_view({"get":"retrieve"})),
+    path("color/", ColorAPIView.as_view({"get": "list"})),
+    path("color/<pk>/", ColorAPIView.as_view({"get": "retrieve"})),
+    path("font/", FontAPIView.as_view({"get": "list"})),
+    path("font/<pk>/", FontAPIView.as_view({"get": "retrieve"})),
+    path("design/", DesignAPIView.as_view({"get": "list"})),
+    path("design/<pk>/", DesignAPIView.as_view({"get": "retrieve"})),
+    path("map_design/", MapDesignAPIView.as_view({"get": "list"})),
+    path("map_design/<pk>/", MapDesignAPIView.as_view({"get": "retrieve"})),
+    path("product_model/", ProductModelAPIView.as_view({"get": "list"})),
+    path("product_model/<pk>/", ProductModelAPIView.as_view({"get": "retrieve"})),
 ]
