080f0725da178232205f8f054a83136a826e838f
alinowrouzii <alinowrouzii@gmail.com>
2022-03-30 20:04:48 +0430

    Add cors conf

--- nightSky/nightSky/settings.py
+++ nightSky/nightSky/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "knox",
+    "corsheaders",
     "tinymce",
     "authentication",
     "blog",
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -131,6 +133,7 @@ REST_KNOX = {
     "EXPIRY_DATETIME_FORMAT": None,
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Tinymce config
 
